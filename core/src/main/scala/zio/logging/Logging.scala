d01762a02ab646f857745197d9d6e8b8f289e393
przemyslaw wierzbicki <rzbikson@gmail.com>
2019-11-03 23:18:44 -0500

    changed approach with context map

--- core/src/main/scala/zio/logging/Logging.scala
+++ core/src/main/scala/zio/logging/Logging.scala
@@ -1,6 +1,8 @@
 package zio.logging
 
-import zio.{ Cause, FiberRef, UIO, ZIO }
+import zio.{Cause, FiberRef, UIO, ZIO}
+
+import scala.reflect.ClassTag
 
 trait AbstractLogging[Message] {
   def logging: AbstractLogging.Service[Any, Message]
@@ -9,8 +11,8 @@ trait AbstractLogging[Message] {
 object AbstractLogging {
 
   trait Service[-R, Message] {
-    def context: LoggingContext[Message]
-    def trace(message: Message): ZIO[R, Nothing, Unit]
+    def loggingContext: ContextMap
+    def trace(message: => Message): ZIO[R, Nothing, Unit]
     def debug(message: Message): ZIO[R, Nothing, Unit]
     def info(message: Message): ZIO[R, Nothing, Unit]
     def warning(message: Message): ZIO[R, Nothing, Unit]
@@ -18,19 +20,42 @@ object AbstractLogging {
     def error(message: Message, cause: Cause[Any]): ZIO[R, Nothing, Unit]
   }
 
-  final class LoggingContext[Message] private (ctx: FiberRef[Map[String, Message]]) {
+  class ContextMap private (private val map: FiberRef[Map[ContextKey[Any], AnyRef]]) {
+    def get[V](key: ContextKey[V]): UIO[V] =
+      map.get.map(
+        _.getOrElse(key.asInstanceOf[ContextKey[Any]], key.initial).asInstanceOf[V]
+      )
 
-    def apply(items: (String, Message)*): UIO[Unit] =
-      ctx.update(_ ++ items).unit
+    def add[V](key: ContextKey[V], value: V): UIO[Unit] =
+      map.update{ oldMap =>
+        val mapKey = key.asInstanceOf[ContextKey[Any]]
+        val maybeOldValue = oldMap.get(mapKey).map(oldValue => key.combine(oldValue.asInstanceOf[V], value))
+        oldMap.updated(mapKey, maybeOldValue.getOrElse(value).asInstanceOf[AnyRef])
+      }.unit
   }
 
-  object LoggingContext {
-
-    def make[Message]: UIO[LoggingContext[Message]] =
-      FiberRef
-        .make(Map.empty[String, Message])
-        .map(new LoggingContext[Message](_))
+  object ContextMap {
+    val empty = FiberRef.make(Map.empty[ContextKey[Any], AnyRef])
+      .map(fiber => new ContextMap(fiber))
   }
+
+  class ContextKey[V] private (
+                                val identifier: String,
+                                val initial: V,
+                                val combine: (V, V) => V,
+                                private[logging] val classTag: ClassTag[V]
+                              )
+
+  object ContextKey {
+    def apply[V](
+                  identifier: String,
+                  initial: V,
+                  combine: (V, V) => V = (_: V, newValue: V) => newValue, // by default replace
+                  )(implicit classTag: ClassTag[V]): ContextKey[V] =
+      new ContextKey(identifier, initial, combine, classTag)
+  }
+
+
 }
 trait Logging extends AbstractLogging[String]
 
