1ee58515297d661b59a057c9ebdef59468cac557
Maxim Schuwalow <maxim.schuwalow@zalando.de>
2019-11-25 12:51:29 +0100

    update span

--- core/src/main/scala/zio/logging/ContextMap.scala
+++ core/src/main/scala/zio/logging/ContextMap.scala
@@ -5,42 +5,80 @@ import zio._
 final class ContextMap private (private val map: FiberRef[ContextMap.CMap]) {
 
   def get[V](key: ContextKey[V]): UIO[V] =
-    map.get.map(ContextMap.get(_)(key))
+    map.get.map(ContextMap.get(_)(key)._2)
 
   def add[V](key: ContextKey[V], value: V): UIO[Unit] =
     map.update(ContextMap.add(_)(key, value)).unit
 
-  def merge(other: ContextMap): UIO[Unit] =
-    for {
-      otherMap <- other.map.get
-      _        <- map.update(ContextMap.merge(_, otherMap))
-    } yield ()
+  def set[V](key: ContextKey[V], value: V): UIO[Unit] =
+    map.update(ContextMap.set(_)(key, value)).unit
+
+  def remove(key: ContextKey[_]): UIO[Unit] =
+    map.update(ContextMap.remove(_)(key)).unit
 
   def locally[R, E, A, V](key: ContextKey[V], value: V)(zio: ZIO[R, E, A]): ZIO[R, E, A] =
     for {
-      current <- map.get
-      result  <- map.locally(ContextMap.add(current)(key, value))(zio)
-    } yield result
+      oldValue <- map.get
+      newValue = ContextMap.add(oldValue)(key, value).mapValues(old => (Nil, old._2))
+      b <- map
+            .set(newValue)
+            .bracket_(
+              for {
+                changes <- map.get.map(_.mapValues(_._1))
+                _       <- map.set(ContextMap.merge(oldValue, changes))
+              } yield ()
+            )(zio)
+    } yield b
 
 }
 
 object ContextMap {
+  sealed trait Action[+T]
+  object Action {
+    final case class Add[T](value: T) extends Action[T]
+    final case class Set[T](value: T) extends Action[T]
+    case object Remove                extends Action[Nothing]
+  }
+
   // underlying untyped map
-  private[ContextMap] type CMap = Map[ContextKey[Any], Any]
+  private[ContextMap] type CMap = Map[ContextKey[Any], (List[Action[Any]], Any)]
 
-  private[ContextMap] def get[V](map: CMap)(key: ContextKey[V]): V =
-    map.getOrElse(key.asInstanceOf[ContextKey[Any]], key.initial).asInstanceOf[V]
+  private[ContextMap] def get[V](map: CMap)(key: ContextKey[V]): (List[Action[V]], V) =
+    map.getOrElse(key.asInstanceOf[ContextKey[Any]], (Nil, key.initial)).asInstanceOf[(List[Action[V]], V)]
 
-  private[ContextMap] def add[V](map: CMap)(key: ContextKey[V], value: V): CMap =
-    map + (key.asInstanceOf[ContextKey[Any]] -> key.combine(get(map)(key), value).asInstanceOf[Any])
+  private[ContextMap] def set[V](map: CMap)(key: ContextKey[V], value: V): CMap = {
+    val (oldChanges, _) = get(map)(key)
+    map + (key.asInstanceOf[ContextKey[Any]] -> (Action.Set(value) :: oldChanges, value)
+      .asInstanceOf[(List[Action[Any]], Any)])
+  }
 
-  private[ContextMap] def merge(first: CMap, second: CMap): CMap =
-    second.foldLeft(first) {
+  private[ContextMap] def add[V](map: CMap)(key: ContextKey[V], value: V): CMap = {
+    val (oldChanges, oldV) = get(map)(key)
+    map + (key.asInstanceOf[ContextKey[Any]] -> (Action.Add(value) :: oldChanges, key.combine(oldV, value))
+      .asInstanceOf[(List[Action[Any]], Any)])
+  }
+
+  private[ContextMap] def remove(map: CMap)(key: ContextKey[_]): CMap = {
+    val (oldChanges, _) = get(map)(key)
+    map + (key.asInstanceOf[ContextKey[Any]] -> (Action.Remove :: oldChanges, key.initial)
+      .asInstanceOf[(List[Action[Any]], Any)])
+  }
+
+  private[ContextMap] def merge(map: CMap, changes: Map[ContextKey[Any], List[Action[Any]]]): CMap =
+    changes.foldLeft(map) {
       case (m, (k, v)) =>
-        add(m)(k, v)
+        val (oldChanges, oldValue) = m.getOrElse(k, (Nil, k.initial))
+        m + (k -> v.foldRight((oldChanges, oldValue)) {
+          case (c, (cs, v1)) =>
+            (c :: cs, c match {
+              case Action.Add(v2) => k.combine(v1, v2)
+              case Action.Set(v2) => v2
+              case Action.Remove  => k.initial
+            })
+        })
     }
 
   val empty = FiberRef
-    .make(Map.empty[ContextKey[Any], AnyRef], merge)
+    .make(Map.empty[ContextKey[Any], (List[Action[Any]], Any)], (c1: CMap, c2: CMap) => merge(c1, c2.mapValues(_._1)))
     .map(fiber => new ContextMap(fiber))
 }
