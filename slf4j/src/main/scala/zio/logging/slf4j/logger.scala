4f92b33294a443732e0fdfb7ff80e69ca1b2d5a1
przemyslaw wierzbicki <rzbikson@gmail.com>
2020-01-18 17:36:00 -0500

    remove old stuff

--- slf4j/src/main/scala/zio/logging/slf4j/logger.scala
+++ slf4j/src/main/scala/zio/logging/slf4j/logger.scala
@@ -10,6 +10,6 @@ object logger {
   def log(level: LogLevel)(line: => String): ZIO[Logger, Nothing, Unit] =
     ZIO.accessM[Logger](_.log(level)(line))
 
-  def locallyAnnotate[A, R <: Logger, E, A1](annotation: LogAnnotation[A], value: A)(zio: ZIO[R, E, A1]) =
+  def locallyAnnotate[A, R <: Logger, E, A1](annotation: LogAnnotation[A], value: A)(zio: ZIO[R, E, A1]): ZIO[Logger with R, E, A1] =
     ZIO.accessM[Logger with R](_.locallyAnnotate[A, R, E, A1](annotation, value)(zio))
 }
