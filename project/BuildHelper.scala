65d183b7d5c7de09e09960888f6aebd6e28c1f54
Scala Steward <me@scala-steward.org>
2021-05-15 21:14:07 +0200

    Update scala-java-time to 2.3.0

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -296,7 +296,7 @@ object BuildHelper {
 
   def jsSettings =
     Seq(
-      libraryDependencies += "io.github.cquiroz" %%% "scala-java-time"      % "2.2.2",
+      libraryDependencies += "io.github.cquiroz" %%% "scala-java-time"      % "2.3.0",
       libraryDependencies += "io.github.cquiroz" %%% "scala-java-time-tzdb" % "2.2.2"
     )
 
