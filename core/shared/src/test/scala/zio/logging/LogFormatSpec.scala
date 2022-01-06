3f38f3754ffdbca133972a948fe810a42b30bb45
Adam Fraser <adam.fraser@gmail.com>
2022-01-06 14:50:08 -0500

    format

--- core/shared/src/test/scala/zio/logging/LogFormatSpec.scala
+++ core/shared/src/test/scala/zio/logging/LogFormatSpec.scala
@@ -10,28 +10,39 @@ object LogFormatSpec extends DefaultRunnableSpec {
     test("line") {
       val format = line
       check(Gen.string) { line =>
-        val result = format.toLogger(ZTraceElement.empty, FiberId.None, LogLevel.Info, () => line, Map.empty, Nil, ZTraceElement.empty)
+        val result = format
+          .toLogger(ZTraceElement.empty, FiberId.None, LogLevel.Info, () => line, Map.empty, Nil, ZTraceElement.empty)
         assertTrue(result == line)
       }
     },
     test("level") {
       val format = level
       check(Gen.elements(LogLevel.Info, LogLevel.Warning, LogLevel.Error, LogLevel.Debug)) { level =>
-        val result = format.toLogger(ZTraceElement.empty, FiberId.None, level, () => "", Map.empty, Nil, ZTraceElement.empty)
+        val result =
+          format.toLogger(ZTraceElement.empty, FiberId.None, level, () => "", Map.empty, Nil, ZTraceElement.empty)
         assertTrue(result == level.label)
       }
     },
     test("level_value") {
       val format = level_value
       check(Gen.elements(LogLevel.Info, LogLevel.Warning, LogLevel.Error, LogLevel.Debug)) { level =>
-        val result = format.toLogger(ZTraceElement.empty, FiberId.None, level, () => "", Map.empty, Nil, ZTraceElement.empty)
+        val result =
+          format.toLogger(ZTraceElement.empty, FiberId.None, level, () => "", Map.empty, Nil, ZTraceElement.empty)
         assertTrue(result == level.syslog.toString)
       }
     },
     test("fiberId") {
       val format = fiberNumber
       check(Gen.int, Gen.int) { (seq, time) =>
-        val result = format.toLogger(ZTraceElement.empty, FiberId(seq, time), LogLevel.Info, () => "", Map.empty, Nil, ZTraceElement.empty)
+        val result = format.toLogger(
+          ZTraceElement.empty,
+          FiberId(seq, time),
+          LogLevel.Info,
+          () => "",
+          Map.empty,
+          Nil,
+          ZTraceElement.empty
+        )
         assertTrue(result == s"zio-fiber-<$seq>")
       }
     },
@@ -65,34 +76,46 @@ object LogFormatSpec extends DefaultRunnableSpec {
     },
     test("enclosing class") {
       val format = enclosingClass
-      val result = format.toLogger(implicitly[ZTraceElement], FiberId.None, LogLevel.Info, () => "", Map.empty, Nil, ZTraceElement.empty)
+      val result = format
+        .toLogger(implicitly[ZTraceElement], FiberId.None, LogLevel.Info, () => "", Map.empty, Nil, ZTraceElement.empty)
       assertTrue(result == "")
     } @@ TestAspect.ignore,
     test("string concat") {
       val format = string("a") + line + string("c")
       check(Gen.string) { line =>
-        val result = format.toLogger(ZTraceElement.empty, FiberId.None, LogLevel.Info, () => line, Map.empty, Nil, ZTraceElement.empty)
+        val result = format
+          .toLogger(ZTraceElement.empty, FiberId.None, LogLevel.Info, () => line, Map.empty, Nil, ZTraceElement.empty)
         assertTrue(result == "a" + line + "c")
       }
     },
     test("spaced") {
       val format = line |-| string("c")
       check(Gen.string) { line =>
-        val result = format.toLogger(ZTraceElement.empty, FiberId.None, LogLevel.Info, () => line, Map.empty, Nil, ZTraceElement.empty)
+        val result = format
+          .toLogger(ZTraceElement.empty, FiberId.None, LogLevel.Info, () => line, Map.empty, Nil, ZTraceElement.empty)
         assertTrue(result == line + " c")
       }
     },
     test("colored") {
       val format = line.color(LogColor.RED)
       check(Gen.string) { line =>
-        val result = format.toLogger(ZTraceElement.empty, FiberId.None, LogLevel.Info, () => line, Map.empty, Nil, ZTraceElement.empty)
+        val result = format
+          .toLogger(ZTraceElement.empty, FiberId.None, LogLevel.Info, () => line, Map.empty, Nil, ZTraceElement.empty)
         assertTrue(result == LogColor.RED.ansi + line + LogColor.RESET.ansi)
       }
     },
     test("fixed") {
       val format = line.fixed(10)
       check(Gen.string) { line =>
-        val result = format.toLogger(ZTraceElement.empty, FiberId.None, LogLevel.Info, () => line, Map.empty, Nil, ZTraceElement.empty)
+        val result = format.toLogger(
+          ZTraceElement.empty,
+          FiberId.None,
+          LogLevel.Info,
+          () => line,
+          Map.empty,
+          Nil,
+          ZTraceElement.empty
+        )
         assertTrue(result.size == 10)
       }
     }
