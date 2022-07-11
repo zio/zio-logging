5c3d766f09f9395b5af1145c0ac2fad8e3b63ea9
Peter Kotula <peto.kotula@yahoo.com>
2022-07-11 16:59:47 +0200

    Example (#467)
    * examples
    
    * examples
    
    Co-authored-by: John A. De Goes <john@degoes.net>

---
+++ examples/src/main/scala/zio/logging/example/Slf4jExampleApp.scala
@@ -0,0 +1,32 @@
+package zio.logging.example
+
+import zio.logging.LogFormat
+import zio.logging.backend.SLF4J
+import zio.{ Cause, ExitCode, Runtime, Scope, URIO, ZIO, ZIOAppDefault }
+
+object Slf4jExampleApp extends ZIOAppDefault {
+
+  private val slf4jLogger = SLF4J.slf4j(zio.LogLevel.Debug, LogFormat.line |-| LogFormat.cause)
+
+  private def ping(address: String): URIO[PingService, Unit] =
+    PingService
+      .ping(address)
+      .foldZIO(
+        e => ZIO.logErrorCause(s"ping: $address - error", Cause.fail(e)),
+        r => ZIO.logInfo(s"ping: $address - result: $r")
+      )
+
+  override def run: ZIO[Scope, Any, ExitCode] = {
+
+    val run: ZIO[PingService, Throwable, ExitCode] =
+      for {
+        _ <- ping("127.0.0.1")
+        _ <- ping("x8.8.8.8")
+      } yield ExitCode.success
+
+    run.provide(
+      LivePingService.layer ++ (Runtime.removeDefaultLoggers >>> slf4jLogger)
+    )
+  }
+
+}
---
+++ examples/src/test/scala/zio/logging/example/PingServiceSpec.scala
@@ -0,0 +1,35 @@
+package zio.logging.example
+
+import zio.test.{ Spec, TestAspect, TestEnvironment, ZIOSpecDefault, ZTestLogger, assertTrue }
+import zio.{ LogLevel, Runtime }
+
+import java.net.UnknownHostException
+
+object PingServiceSpec extends ZIOSpecDefault {
+
+  override def spec: Spec[TestEnvironment, Any] = suite("PingServiceSpec")(
+    test("ping with valid address") {
+      for {
+        pingResult   <- PingService.ping("127.0.0.1")
+        loggerOutput <- ZTestLogger.logOutput
+      } yield assertTrue(pingResult) && assertTrue(loggerOutput.size == 1) && assertTrue(
+        loggerOutput(0).logLevel == LogLevel.Debug && loggerOutput(0).message() == "ping: /127.0.0.1"
+      )
+    },
+    test("fail on invalid address") {
+      for {
+        pingResult   <- PingService.ping("x8.8.8.8").either
+        loggerOutput <- ZTestLogger.logOutput
+      } yield assertTrue(
+        pingResult.isLeft && pingResult.fold(_.isInstanceOf[UnknownHostException], _ => false)
+      ) && assertTrue(loggerOutput.size == 1) && assertTrue(
+        loggerOutput(0).logLevel == LogLevel.Error && loggerOutput(0)
+          .message() == "ping: x8.8.8.8 - invalid address error" && loggerOutput(0).cause.failureOption.exists(
+          _.isInstanceOf[UnknownHostException]
+        )
+      )
+    }
+  ).provideLayer(
+    LivePingService.layer ++ (Runtime.removeDefaultLoggers >>> ZTestLogger.default)
+  ) @@ TestAspect.sequential
+}
