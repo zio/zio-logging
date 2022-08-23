3b3ee70373f7e77f096db90911734620697ef0d1
Peter Kotula <peto.kotula@yahoo.com>
2022-08-23 18:42:28 +0200

    jpl - java platform/system logger backend fixes

--- jpl/src/test/scala/zio/logging/backend/TestJPLogger.scala
+++ jpl/src/test/scala/zio/logging/backend/TestJPLogger.scala
@@ -5,6 +5,7 @@ import zio.logging.backend.TestAppender.{ LogEntry, logLevelMapping }
 import java.text.MessageFormat
 import java.util.ResourceBundle
 import java.util.concurrent.ConcurrentHashMap
+import scala.util.Try
 
 class TestJPLogger(val name: String) extends System.Logger {
   override def getName: String = name
@@ -12,13 +13,16 @@ class TestJPLogger(val name: String) extends System.Logger {
   override def isLoggable(level: System.Logger.Level): Boolean =
     level.getSeverity >= System.Logger.Level.INFO.getSeverity
 
+  private def getString(bundle: ResourceBundle, key: String): String =
+    Option(bundle).flatMap(b => Try(b.getString(key)).toOption).getOrElse(key)
+
   override def log(level: System.Logger.Level, bundle: ResourceBundle, msg: String, thrown: Throwable): Unit = {
     if (isLoggable(level)) {
       val entry = LogEntry(
         name,
         Thread.currentThread().getName,
         logLevelMapping(level),
-        msg,
+        getString(bundle, msg),
         System.currentTimeMillis(),
         Option(thrown).map(_.getMessage)
       )
@@ -28,13 +32,13 @@ class TestJPLogger(val name: String) extends System.Logger {
     ()
   }
 
-  override def log(level: System.Logger.Level, bundle: ResourceBundle, format: String, params: Any*): Unit = {
+  override def log(level: System.Logger.Level, bundle: ResourceBundle, format: String, params: AnyRef*): Unit = {
     if (isLoggable(level)) {
       val entry = LogEntry(
         name,
         Thread.currentThread().getName,
         logLevelMapping(level),
-        MessageFormat.format(format, params),
+        MessageFormat.format(getString(bundle, format), params),
         System.currentTimeMillis(),
         None
       )
