5247cec343701756020f641c8f31484887765e4a
Rituraj <rituraj.khare@knoldus.com>
2023-01-20 18:28:24 +0530

    added loggerSpec for None case

--- core/jvm/src/test/scala/zio/logging/MetricsSpec.scala
+++ core/jvm/src/test/scala/zio/logging/MetricsSpec.scala
@@ -21,9 +21,10 @@ object MetricsSpec extends ZIOSpecDefault {
         warnCounter  <- loggedTotalMetric.tagged(logLevelMetricLabel, LogLevel.Warning.label.toLowerCase).value
         errorCounter <- loggedTotalMetric.tagged(logLevelMetricLabel, LogLevel.Error.label.toLowerCase).value
         fatalCounter <- loggedTotalMetric.tagged(logLevelMetricLabel, LogLevel.Fatal.label.toLowerCase).value
+        clearCounter <- loggedTotalMetric.tagged(logLevelMetricLabel, LogLevel.None.label.toLowerCase).value
       } yield assertTrue(debugCounter.count == 2d) && assertTrue(infoCounter.count == 2d) && assertTrue(
         warnCounter.count == 2d
-      ) && assertTrue(errorCounter.count == 1d) && assertTrue(fatalCounter.count == 0d)).provideLayer(logMetrics)
+      ) && assertTrue(errorCounter.count == 1d) && assertTrue(fatalCounter.count == 0d) && assertTrue(clearCounter.count == 0d)).provideLayer(logMetrics)
     }
   )
 }
