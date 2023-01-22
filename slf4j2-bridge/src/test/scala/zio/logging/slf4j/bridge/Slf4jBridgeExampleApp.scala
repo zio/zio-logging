d28459d3ccab36a1c1246646f960453dd0995f99
Peter Kotula <peto.kotula@yahoo.com>
2023-01-22 19:59:58 +0100

    copyright

--- slf4j2-bridge/src/test/scala/zio/logging/slf4j/bridge/Slf4jBridgeExampleApp.scala
+++ slf4j2-bridge/src/test/scala/zio/logging/slf4j/bridge/Slf4jBridgeExampleApp.scala
@@ -1,14 +1,27 @@
 package zio.logging.slf4j.bridge
 
-import zio.logging.{ LogFormat, consoleJson }
-import zio.{ ExitCode, LogLevel, Runtime, Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer }
+import org.slf4j.{Logger, LoggerFactory}
+import zio.logging.{LogFilter, LogFormat, LoggerNameExtractor, consoleJson}
+import zio.{ExitCode, LogLevel, Runtime, Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
 
 object Slf4jBridgeExampleApp extends ZIOAppDefault {
 
-  private val slf4jLogger = org.slf4j.LoggerFactory.getLogger("SLF4J-LOGGER")
+  private val slf4jLogger: Logger = LoggerFactory.getLogger("SLF4J-LOGGER")
+
+  private val loggerName = LoggerNameExtractor.annotationOrTrace(Slf4jBridge.loggerNameAnnotationKey)
+
+  private val logFilter: LogFilter[String] = LogFilter.logLevelByGroup(
+    LogLevel.Info,
+    loggerName.toLogGroup(),
+    "zio.logging.slf4j" -> LogLevel.Debug,
+    "SLF4J-LOGGER" -> LogLevel.Warning
+  )
 
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Any] =
-    Runtime.removeDefaultLoggers >>> consoleJson(LogFormat.default, LogLevel.Debug) >+> Slf4jBridge.initialize
+    Runtime.removeDefaultLoggers >>> consoleJson(
+      LogFormat.label("name", loggerName.toLogFormat()) + LogFormat.default,
+      logFilter
+    ) >+> Slf4jBridge.initialize
 
   override def run: ZIO[Scope, Any, ExitCode] =
     (for {
