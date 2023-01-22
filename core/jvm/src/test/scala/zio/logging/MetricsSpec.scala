32cbd338fa7e7552fa270eab81ee56bedd565019
Rituraj <rituraj.khare@knoldus.com>
2023-01-23 00:59:52 +0530

    reformat

--- core/jvm/src/test/scala/zio/logging/MetricsSpec.scala
+++ core/jvm/src/test/scala/zio/logging/MetricsSpec.scala
@@ -24,7 +24,8 @@ object MetricsSpec extends ZIOSpecDefault {
         clearCounter <- loggedTotalMetric.tagged(logLevelMetricLabel, LogLevel.None.label.toLowerCase).value
       } yield assertTrue(debugCounter.count == 2d) && assertTrue(infoCounter.count == 2d) && assertTrue(
         warnCounter.count == 2d
-      ) && assertTrue(errorCounter.count == 1d) && assertTrue(fatalCounter.count == 0d) && assertTrue(clearCounter.count == 0d)).provideLayer(logMetrics)
+      ) && assertTrue(errorCounter.count == 1d) && assertTrue(fatalCounter.count == 0d)
+        && assertTrue(clearCounter.count == 0d)).provideLayer(logMetrics)
     }
   )
 }
