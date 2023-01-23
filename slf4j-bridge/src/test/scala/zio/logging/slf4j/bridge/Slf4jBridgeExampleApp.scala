15a273bd852bc8ca8eac1b7c4e72a26fc4db1ef7
Peter Kotula <peto.kotula@yahoo.com>
2023-01-23 10:10:39 +0100

    example fix

--- slf4j-bridge/src/test/scala/zio/logging/slf4j/bridge/Slf4jBridgeExampleApp.scala
+++ slf4j-bridge/src/test/scala/zio/logging/slf4j/bridge/Slf4jBridgeExampleApp.scala
@@ -1,12 +1,11 @@
 package zio.logging.slf4j.bridge
 
-import org.slf4j.{ Logger, LoggerFactory }
 import zio.logging.{ LogFilter, LogFormat, LoggerNameExtractor, consoleJson }
 import zio.{ ExitCode, LogLevel, Runtime, Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer }
 
 object Slf4jBridgeExampleApp extends ZIOAppDefault {
 
-  private val slf4jLogger: Logger = LoggerFactory.getLogger("SLF4J-LOGGER")
+  private val slf4jLogger = org.slf4j.LoggerFactory.getLogger("SLF4J-LOGGER")
 
   private val loggerName = LoggerNameExtractor.annotationOrTrace(Slf4jBridge.loggerNameAnnotationKey)
 
