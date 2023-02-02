eef98c85d1533cea886f2a22f76e1f72825d24f8
Ashish Chaudhary <77047591+aashish2054@users.noreply.github.com>
2023-02-02 15:07:20 +0530

    Added implementation for testTrace variable (#641)

--- core/jvm/src/test/scala/zio/logging/MetricsSpec.scala
+++ core/jvm/src/test/scala/zio/logging/MetricsSpec.scala
@@ -18,6 +18,7 @@ object MetricsSpec extends ZIOSpecDefault {
         _            <- TestService.testInfo
         _            <- TestService.testWarning
         _            <- TestService.testError
+        _            <- TestService.testTrace
         debugCounter <- loggedTotalMetric.tagged(logLevelMetricLabel, LogLevel.Debug.label.toLowerCase).value
         infoCounter  <- loggedTotalMetric.tagged(logLevelMetricLabel, LogLevel.Info.label.toLowerCase).value
         warnCounter  <- loggedTotalMetric.tagged(logLevelMetricLabel, LogLevel.Warning.label.toLowerCase).value
@@ -28,7 +29,7 @@ object MetricsSpec extends ZIOSpecDefault {
       } yield assertTrue(debugCounter.count == 2d) && assertTrue(infoCounter.count == 2d) && assertTrue(
         warnCounter.count == 2d
       ) && assertTrue(errorCounter.count == 2d) && assertTrue(fatalCounter.count == 0d)
-        && assertTrue(clearCounter.count == 0d) && assertTrue(traceCounter.count == 1d))
+        && assertTrue(clearCounter.count == 0d) && assertTrue(traceCounter.count == 2d))
         .provideLayer(logMetrics)
     }
   )
