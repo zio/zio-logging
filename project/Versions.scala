3ceb219d4f639a9c093f39a805c458c72e6e23f4
Scala Steward <scala_steward@virtuslab.com>
2023-03-02 20:18:14 +0000

    Update scala-java-time to 2.5.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,7 +5,7 @@ object Versions {
   val logback2Version               = "1.4.5"
   val scalaCollectionCompatVersion  = "2.9.0"
   val logstashLogbackEncoderVersion = "6.6"
-  val scalaJavaTimeVersion          = "2.4.0"
+  val scalaJavaTimeVersion          = "2.5.0"
   val zioMetricsConnectorsVersion   = "2.0.4"
   val log4jVersion                  = "2.19.0"
 }
