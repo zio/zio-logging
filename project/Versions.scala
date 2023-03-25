d15cbef5fcbb9556ac9a05f56399f15d2a372f8f
Scala Steward <scala_steward@virtuslab.com>
2023-03-25 01:06:50 +0000

    Update logback-classic to 1.2.12

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val slf4jVersion                  = "1.7.36"
   val slf4j2Version                 = "2.0.7"
-  val logbackVersion                = "1.2.11"
+  val logbackVersion                = "1.2.12"
   val logback2Version               = "1.4.6"
   val scalaCollectionCompatVersion  = "2.9.0"
   val logstashLogbackEncoderVersion = "6.6"
