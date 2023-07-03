758454b175b96fd633b7d603acb60200adec3d89
Scala Steward <scala_steward@virtuslab.com>
2023-07-03 01:27:06 +0000

    Update zio-metrics-connectors to 2.1.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   val scalaCollectionCompatVersion  = "2.11.0"
   val logstashLogbackEncoderVersion = "6.6"
   val scalaJavaTimeVersion          = "2.5.0"
-  val zioMetricsConnectorsVersion   = "2.0.8"
+  val zioMetricsConnectorsVersion   = "2.1.0"
   val zioConfig                     = "4.0.0-RC16"
   val zioParser                     = "0.1.9"
   val log4jVersion                  = "2.19.0"
