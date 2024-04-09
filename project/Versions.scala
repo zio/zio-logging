fb2852e8f8100c6132771f86e3d9f7a1785f805d
Scala Steward <scala_steward@virtuslab.com>
2024-04-09 02:59:56 +0000

    Update zio-http to 3.0.0-RC6

--- project/Versions.scala
+++ project/Versions.scala
@@ -10,6 +10,6 @@ object Versions {
   val zioConfig                     = "4.0.1"
   val zioParser                     = "0.1.9"
   val zioPrelude                    = "1.0.0-RC23"
-  val zioHttp                       = "3.0.0-RC4"
+  val zioHttp                       = "3.0.0-RC6"
   val log4jVersion                  = "2.23.1"
 }
