1372dfbd6fe84905867518fde0f4989d18d571ed
Scala Steward <scala_steward@virtuslab.com>
2024-04-13 18:20:33 +0000

    Update slf4j-api to 2.0.13

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val slf4jVersion                  = "1.7.36"
-  val slf4j2Version                 = "2.0.9"
+  val slf4j2Version                 = "2.0.13"
   val logbackVersion                = "1.2.13"
   val logback2Version               = "1.4.12"
   val scalaCollectionCompatVersion  = "2.11.0"
