8d2671b0b3a57ac21311f43257e8983a94a887da
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-08-30 10:33:20 +0200

    Update zio-prelude to 1.0.0-RC30 (#879)
    Co-authored-by: Peter Kotula <peto.kotula@yahoo.com>

--- project/Versions.scala
+++ project/Versions.scala
@@ -9,7 +9,7 @@ object Versions {
   val zioMetricsConnectorsVersion   = "2.3.1"
   val zioConfig                     = "4.0.2"
   val zioParser                     = "0.1.10"
-  val zioPrelude                    = "1.0.0-RC29"
+  val zioPrelude                    = "1.0.0-RC30"
   val zioHttp                       = "3.0.0-RC9"
   val log4jVersion                  = "2.23.1"
 }
