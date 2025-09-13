126819ece6814cb14eae37be6821f613ad60f6bd
Scala Steward <scala_steward@virtuslab.com>
2025-09-13 23:57:09 +0000

    Update zio-config-typesafe to 4.0.5

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,7 +7,7 @@ object Versions {
   val logstashLogbackEncoderVersion = "6.6"
   val scalaJavaTimeVersion          = "2.6.0"
   val zioMetricsConnectorsVersion   = "2.5.0"
-  val zioConfig                     = "4.0.4"
+  val zioConfig                     = "4.0.5"
   val zioParser                     = "0.1.11"
   val zioPrelude                    = "1.0.0-RC41"
   val zioHttp                       = "3.5.0"
