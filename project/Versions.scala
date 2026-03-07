deb8e55b1366229b38651076c710c165dc08edf2
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-03-07 09:01:44 +0100

    Update zio-http to 3.9.0 (#1069)

--- project/Versions.scala
+++ project/Versions.scala
@@ -10,6 +10,6 @@ object Versions {
   val zioConfig                     = "4.0.6"
   val zioParser                     = "0.1.11"
   val zioPrelude                    = "1.0.0-RC46"
-  val zioHttp                       = "3.8.1"
+  val zioHttp                       = "3.9.0"
   val log4jVersion                  = "2.25.3"
 }
